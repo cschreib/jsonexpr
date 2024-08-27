4fe3b0aa382fad0a9804905eb4e47181f897edab
Corentin Schreiber <github@corentin.net>
2024-08-27 12:10:00 +0100

    Add missing tests for base.cpp

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -4,6 +4,7 @@
 #include <snitch/snitch_macros_test_case.hpp>
 
 using namespace jsonexpr;
+using namespace snitch::matchers;
 
 namespace snitch {
 bool append(snitch::small_string_span ss, const json& j) {
@@ -27,6 +28,38 @@ bool append(snitch::small_string_span ss, const expected<T, E>& r) {
     }
 }
 } // namespace snitch
+
+TEST_CASE("type name", "[general]") {
+    CHECK(get_type_name("true"_json) == "bool");
+    CHECK(get_type_name(true) == "bool");
+
+    CHECK(get_type_name("null"_json) == "null");
+
+    CHECK(get_type_name("1"_json) == "int");
+    CHECK(get_type_name("-1"_json) == "int");
+    CHECK(get_type_name(std::int64_t{1}) == "int");
+
+    CHECK(get_type_name("1.0"_json) == "float");
+    CHECK(get_type_name(1.0) == "float");
+
+    CHECK(get_type_name(R"("abc")"_json) == "string");
+    CHECK(get_type_name(std::string("abc")) == "string");
+
+    CHECK(get_type_name("[1,2,3]"_json) == "array");
+    CHECK(get_type_name(std::vector<json>{}) == "array");
+
+    CHECK(get_type_name(R"({"a":1})"_json) == "object");
+    CHECK(get_type_name(std::unordered_map<std::string, json>{}) == "object");
+}
+
+TEST_CASE("format error", "[general]") {
+    const auto expression   = "1+x";
+    const auto the_error    = error{2, 1, "unknown variable"};
+    const auto error_string = format_error(expression, the_error);
+
+    CHECK(error_string == contains_substring{expression});
+    CHECK(error_string == contains_substring{the_error.message});
+}
 
 TEST_CASE("string literal", "[string]") {
     SECTION("bad") {
